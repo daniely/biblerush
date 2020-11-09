7192ebde16c6478cbabc076e76af02fca685beda
Daniel <daniel.kiros@gmail.com>
2020-11-08 23:03:07 -0800

    fix fonts, more style updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   purge: [],
   theme: {
     fontFamily: {
-      'sans': ['Open Sans', 'Helvetica', 'Arial', 'sans-serif']
+      'sans': [
+        'Open Sans',
+        ...defaultTheme.fontFamily.sans,
+      ],
+      'serif': [defaultTheme.fontFamily.serif]
     },
     extend: {},
   },
