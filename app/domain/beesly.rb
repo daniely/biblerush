607411fbff3938750edfba0103d7f266bb4e4bea
Daniel <daniel.kiros@gmail.com>
2020-11-28 14:00:54 -0800

    add helper to organize scheduling notifications

---
+++ app/domain/beesly.rb
@@ -0,0 +1,64 @@
+# the "office admin" helps create and manage reading plan schedules
+class Beesly
+  def create_subscription(user_id:, reading_plan_id:)
+    now = Time.now.utc
+    sub = Subscription.create(
+      user_id: user_id,
+      reading_plan_id: reading_plan_id,
+      send_at: now,
+      active: true
+    )
+    if sub.save
+      # TODO: send email
+      # Mailer.mail
+
+      # log that notification was sent
+      sub.plan_jobs.create(
+        plan_day: 1,
+        scheduled_for: now,
+        sent_at: now
+      )
+    end
+  end
+
+  # if this is last day of plan
+  #   mark the subscription complete (incomplete)
+  # else
+  #   if current plan has been read
+  #     send next day's passages
+  #   else
+  #     send same day passage
+  #   end
+  # end
+  def schedule_next_reading(plan_job_id:)
+    job = PlanJob.find(plan_job_id)
+    plan_day = job.plan_day
+    next_plan_day = plan_day + 1
+    max_days = job.subscription.reading_plan.days
+    is_reading_plan_done = next_plan_day == max_days
+    if is_reading_plan_done
+      job.subscription.active = false
+    else
+      schedule_for = Time.now.utc + 1.day
+      set_plan_day = if job.read_at.blank?
+                       plan_day
+                     else
+                       next_plan_day
+                     end
+      # TODO: schedule for next day at same `send_at` time
+      job.subscription.plan_jobs.create(
+        plan_day: set_plan_day,
+        scheduled_for: schedule_for
+      )
+      # TODO: schedule the worker
+    end
+    job.save!
+  end
+
+  # mark read and then schedule next reading
+  def mark_read!(plan_job_id:)
+    job = PlanJob.find(plan_job_id)
+    job.read_at = Time.now.utc
+    job.save!
+  end
+end
--- app/models/plan_job.rb
+++ app/models/plan_job.rb
@@ -1,3 +1,7 @@
+# to keep track of reading plan email scheduled jobs
+#
+# if `sent_at` is nil we know the job exists but didn't send out yet
 class PlanJob < ApplicationRecord
+  self.implicit_order_column = "created_at"
   belongs_to :subscription
 end
