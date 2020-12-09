0542f6e6ce4501c972c4093972eee9d46a6cc34b
Daniel <daniel.kiros@gmail.com>
2020-12-09 10:27:44 -0800

    add stimulus.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,4 +1,11 @@
 import '../stylesheets/application.scss'
+import { Application } from "stimulus"
+import { definitionsFromContext } from "stimulus/webpack-helpers"
+
+// for stimulus.js
+const application = Application.start()
+const context = require.context("./controllers", true, /\.js$/)
+application.load(definitionsFromContext(context))
 
 // This file is automatically compiled by Webpack, along with any other files
 // present in this directory. You're encouraged to place your actual application logic in
