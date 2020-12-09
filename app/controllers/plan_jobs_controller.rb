b9c9130e2a6108727481e78d31607ae9977e0fbe
Daniel <daniel.kiros@gmail.com>
2020-12-09 15:05:54 -0800

    don't require login for reading passages page

--- app/controllers/plan_jobs_controller.rb
+++ app/controllers/plan_jobs_controller.rb
@@ -1,4 +1,6 @@
 class PlanJobsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:show]
+
   def show
     plan_job = PlanJob.find(params[:id])
     @reading_plan_detail = plan_job.subscription.reading_plan.reading_plan_details.find_by(day: plan_job.plan_day)
