99356825486836750019834a6171fa3e6be5a179
Daniel <daniel.kiros@gmail.com>
2020-12-09 14:51:14 -0800

    trying to get heroku deploy to work

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,11 @@ import '../stylesheets/application.scss'
 import { Application } from "stimulus"
 import { definitionsFromContext } from "stimulus/webpack-helpers"
 
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
+
 // for stimulus.js
 const application = Application.start()
 const context = require.context("./controllers", true, /\.js$/)
@@ -13,17 +18,6 @@ application.register('alert', Alert)
 application.register('modal', Modal)
 application.register('popover', Popover)
 application.register('tabs', Tabs)
-
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
-
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
-
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
