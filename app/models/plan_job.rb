77554167a2afb489de903d864f4e1d040228a2e9
Daniel <daniel.kiros@gmail.com>
2020-12-10 22:46:48 -0800

    helper method

--- app/models/plan_job.rb
+++ app/models/plan_job.rb
@@ -4,4 +4,9 @@
 class PlanJob < ApplicationRecord
   self.implicit_order_column = "created_at"
   belongs_to :subscription
+
+  # helper method so we don't end up with circular dependencies
+  def reading_plan_detail
+    subscription.reading_plan.reading_plan_details.find_by(day: plan_day)
+  end
 end
