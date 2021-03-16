ef00a1812b76a017cadd2f72555ff23e606192a8
Daniel <daniel.kiros@gmail.com>
2021-03-16 14:32:43 -0700

    style flash messages

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,7 +15,18 @@ module.exports = {
       colors: {
         primary: '#1BACAC',
         'primary-light': '#D1EEEE',
-        teal: colors.teal
+        teal: colors.teal,
+        orange: {
+          100: '#fffaf0',
+          200: '#feebc8',
+          300: '#fbd38d',
+          400: '#f6ad55',
+          500: '#ed8936',
+          600: '#dd6b20',
+          700: '#c05621',
+          800: '#9c4221',
+          900: '#7b341e',
+        }
       },
     },
   },
