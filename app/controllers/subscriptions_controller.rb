d8fa62334ff77eafd3014026204c0a44083accd0
Daniel <daniel.kiros@gmail.com>
2021-04-07 10:43:41 -0700

    show all days of plan

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -10,6 +10,12 @@ class SubscriptionsController < ApplicationController
 
   def show
     @subscription = Subscription.find_by(id: params[:id])
+    completed_days = @subscription.plan_jobs.map(&:plan_day)
+    # array of [day, passage ref, completed]
+    @progress = @subscription.reading_plan.reading_plan_details.map{ |a|
+      [a.day, a.detailed_reference, completed_days.include?(a.day)]
+    }
+
     if @subscription.user.id != current_user.id
       redirect_to user_root_path
     end
