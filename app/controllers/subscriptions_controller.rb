79f6e2e9a32c31ea38889220e60711f57b58552e
Daniel <daniel.kiros@gmail.com>
2021-04-07 16:20:20 -0700

    use image for radio button

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -10,13 +10,14 @@ class SubscriptionsController < ApplicationController
 
   def show
     @subscription = Subscription.find_by(id: params[:id])
+    #[true/false, plan_job id]
     completed_days = {}.tap do |h|
-      @subscription.plan_jobs.map{ |job| h[job.plan_day] = job.read_at.present? }
+      @subscription.plan_jobs.map{ |job| h[job.plan_day] = [job.read_at.present?, job.id] }
     end
     # array of [day, passage ref, completed_days]
     # completed_days => nil=no job  true=day has been read  false=day not read yet
     @progress = @subscription.reading_plan.reading_plan_details.order(:day).map{ |a|
-      [a.day, a.detailed_reference, completed_days[a.day]]
+      [a.day, a.detailed_reference, completed_days[a.day].try(:[], 0), completed_days[a.day].try(:[], 1)]
     }
 
     if @subscription.user.id != current_user.id
