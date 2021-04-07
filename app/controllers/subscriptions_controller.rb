3de9ccfcb86d546cf3451688d7d860fd16ac4afd
Daniel <daniel.kiros@gmail.com>
2021-04-07 16:20:31 -0700

    comments

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -10,11 +10,17 @@ class SubscriptionsController < ApplicationController
 
   def show
     @subscription = Subscription.find_by(id: params[:id])
-    #[true/false, plan_job id]
+    # hash of [true/false, plan_job id]
+    # e.g {1=>[true, "3c528950-e48c-44cb-affa-85bbb0474348"], 2=>[false, "d3ccb63d-0910-4192-8387-fc91bb18a76a"]}
     completed_days = {}.tap do |h|
       @subscription.plan_jobs.map{ |job| h[job.plan_day] = [job.read_at.present?, job.id] }
     end
-    # array of [day, passage ref, completed_days]
+    # [
+    #   plan day,
+    #   passage reference,
+    #   true/false/nil,
+    #   plan ID
+    # ]
     # completed_days => nil=no job  true=day has been read  false=day not read yet
     @progress = @subscription.reading_plan.reading_plan_details.order(:day).map{ |a|
       [a.day, a.detailed_reference, completed_days[a.day].try(:[], 0), completed_days[a.day].try(:[], 1)]
