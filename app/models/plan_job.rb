96dbc9550566bed8179c9fbd7a88c12590994e0e
Daniel <daniel.kiros@gmail.com>
2021-04-20 10:41:33 -0700

    explain how PlanJob is used/interpreted

--- app/models/plan_job.rb
+++ app/models/plan_job.rb
@@ -3,7 +3,14 @@
 # it's expected there will be multiple PlanJob entries for the same passage/day - for
 # when people do not read their passages.
 #
+# we allow multiple PlanJob entries for stats and metrics
+#
 # if `sent_at` is nil we know the job exists but didn't send out yet
+#
+# but if `sent_at` is nil while `read_at` is present, it means we never sent out
+# a reading plan email but they directly read it from the website. do not send emails
+# for plans that have been read.
+#
 class PlanJob < ApplicationRecord
   self.implicit_order_column = "created_at"
   belongs_to :subscription
