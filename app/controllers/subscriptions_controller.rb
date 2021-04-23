bc96f63dc374d11cd83a00a2e80392b373a2a348
Daniel <daniel.kiros@gmail.com>
2021-04-23 10:13:06 -0700

    handle unauthenticated users pausing reading plans

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -28,8 +28,21 @@ class SubscriptionsController < ApplicationController
   end
 
   # pause an active reading plan
+  #
+  # if `id` is present it means an un-authenticated user is trying to pause. so do an
+  # extra check to prevent malicious mass pausing of plans
   def pause
     subscription = Subscription.find(params[:subscription_id])
+    if params[:id].present?
+      plan_job = PlanJob.find_by(id: params[:id])
+      message = if plan_job.blank?
+                  "Sorry, you cannot do that."
+                else
+                  "Your reading plan has been paused."
+                end
+      redirect_to message_path(m: message) and return
+    end
+
     subscription.active = false
     subscription.save!
     redirect_to subscription_path(subscription), notice: 'Reading plan paused.'
